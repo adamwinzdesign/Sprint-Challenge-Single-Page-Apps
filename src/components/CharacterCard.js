418a5e3de337f4fce7faf245114b275090c8f9cc
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-18 18:22:52 -0500

    debugging CharacterList

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
+import '../index.css';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div className="card-wrapper">
+			<img src={props.character.image} alt="A Rick and Morty character" />
+      <h1>{props.character.name}</h1>
+      <h2>Species: {props.character.species}</h2>
+      <h2>Status: {props.character.status}</h2>
+		</div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,57 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    // Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+			.then(response => {
+        console.log(response)
+        const characters = response.data.results.filter(character => 
+          character.name.toLowerCase().includes(query.toLowerCase)()
+        );
+        setCharacters(characters);
+        // setCharacters(response.data.results);
+  		})
+			.catch(error => {
+				console.log(error);
+			});
+  }, [query]);
 
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  }
+  
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h1>Check out your favorite characters!</h1>
+      <form className = 'search'>
+        <input
+          type = 'text'
+          onChange = {handleInputChange}
+          value = {query}
+          name = 'name'
+          tabIndex = '0'
+          className = 'prompt search-name'
+          placeholder = 'Search by Name'
+          autoComplete = 'off'
+        />
+      </form>
+      <div className = 'results'>
+        {characters.map(data => {
+          return (
+            <CharacterCard key={data.index} character={data.character} />
+          )
+        })}
+      </div>
+      {/* {characters.map((character, index) => {
+        return <CharacterCard key={index} character={character} />
+        })} */}
     </section>
   );
 }
